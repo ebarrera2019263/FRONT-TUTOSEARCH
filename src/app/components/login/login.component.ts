f009bb8f7e93c566b5d5cc9fe7d8f4927462c2af
Pablo Javier Alemán Velásquez <paleman-2019222@kinal.edu.gt>
2021-07-20 19:58:31 -0600

    login semi hecho

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserServiceService } from '../../services/restUser/user-service.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user: User;
+  public token:string;
+  public message;
 
-  constructor() { }
+  constructor(private restUser: UserServiceService, private router: Router) { 
+    this.user = new User('','','','','','','','')
+  }
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    this.restUser.login(this.user, 'true').subscribe((res:any)=>{
+      this.message = res.message;
+      if(!res.token){
+        alert(this.message);
+      }else{
+        delete res.user.password;
+        this.token = res.token;
+        if(this.token.length <= 0){
+          alert('El token no se generó o capturó de manera correcta');
+        }else{
+          localStorage.setItem('token', this.token);
+          localStorage.setItem('user', JSON.stringify(res.user));
+          localStorage.setItem('role', JSON.stringify(res.role));    
+          console.log(res.user, res.token);
+          alert('Usuario logeado exitosamente');
+          this.router.navigateByUrl('home')
+        }
+      }
+    },
+    error=> this.message = error.error.message
+    )     
   }
 
 }
